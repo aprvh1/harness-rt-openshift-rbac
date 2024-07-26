832323eb02f3764bd78229dd266acd88c736d5e3
aprvh1 <aprvh1@gmail.com>
2024-07-26 11:10:32 +0530

    checking

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -e
+set -x
 
 #curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
 curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
 
-chmod 777 hce-api-saas
+ls -lh hce-api-saas
+chmod +x hce-api-saas
 
 output=$(./hce-api-saas generate --api launch-experiment --account-id=${ACCOUNT_ID} \
 --project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
---api-key ${API_KEY} --file-name hce-api.sh | jq -r '.data.runChaosExperiment.notifyID')
+--api-key ${API_KEY} --file-name hce-api.sh )
 
 echo ${output}
