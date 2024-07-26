d7a64a372c4352bb6eafda7149b06e3459f7acd6
aprvh1 <aprvh1@gmail.com>
2024-07-26 14:50:51 +0530

    final test run with increased timeout on gitlab

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-set -x
+set -e
 
 #curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
 #curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
 curl  https://app.harness.io/public/shared/tools/chaos/hce-cli/0.0.4/hce-cli-0.0.4-linux-amd64 -o hce-api-saas
 
-ls -lh hce-api-saas
 chmod +x hce-api-saas
 
-# output=$(./hce-api-saas generate --api launch-experiment --account-id=${ACCOUNT_ID} \
-# --project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
-# --api-key ${API_KEY} --file-name hce-api.sh | jq -r '.data.runChaosExperiment.notifyID')
+output=$(./hce-api-saas generate --api launch-experiment --account-id=${ACCOUNT_ID} \
+--project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
+--api-key ${API_KEY} --file-name hce-api.sh | jq -r '.data.runChaosExperiment.notifyID')
 
-./hce-api-saas generate --api launch-experiment --account-id=${ACCOUNT_ID} --project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} --api-key ${API_KEY} --file-name hce-api.sh
-
-cat hce-api.sh
-
-# echo ${output}
+echo ${output}
