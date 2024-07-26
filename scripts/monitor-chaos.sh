92884f42521890e9191908121e7fceb04b67232d
aprvh1 <aprvh1@gmail.com>
2024-07-26 11:46:24 +0530

    binary

--- scripts/monitor-chaos.sh
+++ scripts/monitor-chaos.sh
@@ -3,7 +3,8 @@
 set -e
 
 #curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
-curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
+#curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
+curl  https://app.harness.io/public/shared/tools/chaos/hce-cli/0.0.4/hce-cli-0.0.4-linux-amd64 -o hce-api-saas
 
 chmod +x hce-api-saas
 
