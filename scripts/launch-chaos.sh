ea345a64e0e8107df27501c08391712c79c2b26c
aprvh1 <aprvh1@gmail.com>
2024-07-26 10:54:37 +0530

    checking

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 #curl -sL https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api-saas
 curl -sL https://storage.googleapis.com/hce-api/hce-api-linux-amd64 -o hce-api-saas
 
-chmod +x hce-api-saas
+chmod 777 hce-api-saas
 
 output=$(./hce-api-saas generate --api launch-experiment --account-id=${ACCOUNT_ID} \
 --project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
